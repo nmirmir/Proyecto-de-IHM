33b2e8f4ada125a8ece3ec5c25c9df6cb4012144
Nicolás Miró <nicolasmiromira@gmail.com>
2024-11-12 16:46:46 +0100

    ya hay firstpage seee

--- proyecto_hmi/lib/main.dart
+++ proyecto_hmi/lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'views/first_page.dart';
 
 void main() {
-  print('hello world');
+  runApp(MyApp());
 }
+
+
+
+class MyApp extends StatelessWidget {
+  
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Practice 1',home: FirstPage(),);
+  }
+}
+
---
+++ proyecto_hmi/lib/views/first_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+  @override
+  Widget build(BuildContext context) => Container(
+    color: Colors.blue,
+    child: Text('Hello World'),
+    );
+  
+}
